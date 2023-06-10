d4db7ecc0be0266d53397f78ec5728c5fa0b2010
DMisztal <dominik.misztal@onet.eu>
2023-06-10 19:43:33 +0200

    Improved purge command

--- src/commands/moderation/purge.ts
+++ src/commands/moderation/purge.ts
@@ -1,15 +1,29 @@
 //@ts-nocheck
 import { CommandInteraction, Options, SlashCommandBuilder } from 'discord.js';
 import { Command } from '../../interfaces/Command';
-import { clearMessages } from './purge_help';
 
 export const purge: Command  = {
 	data: new SlashCommandBuilder()
 		.setName('purge')
-		.setDescription('Deletes messages in bulk'),
+		.setDescription('Deletes messages in bulk')
+		.addStringOption((option) =>
+			option
+				.setName("amount")
+				.setDescription("How many messages to delete")
+				.setRequired(true)),
 	run: async(interaction: CommandInteraction) => {
-		console.log(interaction.channel?.id);
-		interaction.channel.bulkDelete(10);
-		interaction.reply("Cleared messages!");
+		var amount = +interaction.options.getString("amount", true);
+		var replyString : String;
+		if(amount > 100){
+			// interaction.reply("Too many messages, choose less than 100");	
+			replyString = "Too many messages, choose less than 100";
+		} else if(amount <= 1){
+			// interaction.reply("Too few messages, choose more than 1");	
+			replyString = "Too few messages, choose more than 1";
+		} else{
+			interaction.channel.bulkDelete(amount);
+		}
+		replyString = "Deleted messages";
+		interaction.reply(replyString);
 	},
 };
