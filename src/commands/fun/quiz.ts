7bcd557b5bfd1132106f578587797833d6bff645
DMisztal <dominik.misztal@onet.eu>
2023-06-19 19:47:33 +0200

    Update

--- src/commands/fun/quiz.ts
+++ src/commands/fun/quiz.ts
@@ -23,8 +23,7 @@ export const question: Command  = {
             password: 'helpie123',
             database: 'quiz',
         });
-		const question : number;
-		question = randomInt(5);
+		const question : number = randomInt(5);
 		const sqlQuery = "Select * From easy_questions where ID = " + question;
         database.connect();
 		const queryResult : string;
